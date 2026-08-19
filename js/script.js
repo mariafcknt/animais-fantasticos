9b4368408d5b9ecbaa0a46855abecb51d7450689
Maria <mariafcknt@outlook.com>
2026-08-19 11:27:09 -0300

    fetch bitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropdownMenu from './modules/dropdown-menu.js'
 import initMenuMobile from './modules/menu-mobile.js'
 import initFuncionamento from './modules/funcionamento.js'
 import fetchAnimais from './modules/fetch-animais.js'
-import initFetchBitcoin from './modules/fetch-bitcoin.js'
+import fetchBitcoin from './modules/fetch-bitcoin.js'
 import initAnimacaoScroll from './modules/scroll-animacao.js'
 
 // import { teste1, teste2 } from "./modules/teste.js";
@@ -35,7 +35,7 @@ tooltip.init()
 initDropdownMenu()
 initMenuMobile()
 initFuncionamento()
-initFetchBitcoin()
 initAnimacaoScroll()
 
 fetchAnimais('../../animais-api.json', '.numeros-grid')
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco')
